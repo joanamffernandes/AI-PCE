0a93aa08f7055bc8f8a6114851192d019d69860a
cvmota <102318987+cvmota@users.noreply.github.com>
2023-03-23 20:18:11 +0000

    - Corrigir o problema de ficar sem a imagem de fundo - Mover o login para dentro da pasta pages - Apagar imagens não utilizadas - Mudar o titulo da página

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import Login from './login/Login';
-import Home from "./pages/Home";
+import Login from './pages/login/Login';
+import Home from "./pages/home/Home";
 
 
 export default function App() {
--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,4 +1,5 @@
 import './Home.css';
+
 function Home() {
     return (
         <h1>Home</h1>
